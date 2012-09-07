7a3a094f7ed3a3fb21d4821e1b139ec00ab300dc
Sam Baumgarten <sbaumgarten@makrr.com>
2012-09-07 12:00:39 -0700

    Initial Commit

---
+++ MightBuy/ViewController.m
@@ -0,0 +1,34 @@
+//
+//  ViewController.m
+//  MightBuy
+//
+//  Created by Sam Baumgarten on 9/7/12.
+//  Copyright (c) 2012 Makrr. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)viewDidUnload
+{
+    [super viewDidUnload];
+    // Release any retained subviews of the main view.
+}
+
+- (BOOL)shouldAutorotateToInterfaceOrientation:(UIInterfaceOrientation)interfaceOrientation
+{
+    return (interfaceOrientation != UIInterfaceOrientationPortraitUpsideDown);
+}
+
+@end
